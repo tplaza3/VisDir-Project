38fa9a047157335f544430f8670563d3f36a5edb
Tony Plaza <tplaza3@gmail.com>
2014-10-01 11:44:25 -0500

    add bcrypt and update user model

--- VisDir/app/models/user.rb
+++ VisDir/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password_digest, :pro
+  attr_accessible :email, :name, :password, :password_confirmation, :pro
   
   has_many :listings
+  
+  has_secure_password
 end
