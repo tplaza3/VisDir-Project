dd4c5dfca3c9e74b4c7dc1da6034e54375e2a48e
Tony Plaza <tplaza3@gmail.com>
2014-10-01 09:23:35 -0500

    add carrierwave migrate the listing table

--- VisDir/app/models/listing.rb
+++ VisDir/app/models/listing.rb
@@ -2,4 +2,6 @@ class Listing < ActiveRecord::Base
   attr_accessible :budget, :category, :listing_image, :location, :title, :user_id
   
   belongs_to :user
+  
+  mount_uploader :listing_image, ImageUploader
 end
--- VisDir/db/schema.rb
+++ VisDir/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140930215027) do
+ActiveRecord::Schema.define(:version => 20140930215520) do
+
+  create_table "listings", :force => true do |t|
+    t.string   "title"
+    t.string   "location"
+    t.string   "budget"
+    t.string   "category"
+    t.string   "listing_image"
+    t.integer  "user_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
