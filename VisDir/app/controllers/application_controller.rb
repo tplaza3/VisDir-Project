401a85e1af34f9c98ba3a9d1accbe1d22904063d
Tony Plaza <tplaza3@gmail.com>
2014-10-02 22:01:09 -0500

    add authentication

--- VisDir/app/controllers/application_controller.rb
+++ VisDir/app/controllers/application_controller.rb
@@ -2,25 +2,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   # before_filter :authorize
- #
- #  def authorize
- #    if current_user.nil?
- #      redirect_to login_path, :alert => "You need to log in first!"
- #    end
- #  end
- #
- #
- #  def current_user
- #    # binding.pry
- #    if session[:user_id]
- #      @current_user ||= User.find_by_id(session[:user_id])
- #      # binding.pry
- #      # if @current_user.nil?
- #  #       reset_session
- #  #       redirect_to login_path, :alert => "You need to log in first!"
- #  #     end
- #    end
- #  end
- #
+
+  def authorize
+    if current_user.nil?
+      redirect_to login_path, :alert => "You need to log in first!"
+    end
+  end
+
+
+  def current_user
+    # binding.pry
+    if session[:user_id]
+      @current_user ||= User.find_by_id(session[:user_id])
+      # binding.pry
+      # if @current_user.nil?
+  #       reset_session
+  #       redirect_to login_path, :alert => "You need to log in first!"
+  #     end
+    end
+  end
+
   helper_method :current_user
 end
--- VisDir/app/controllers/logins_controller.rb
+++ VisDir/app/controllers/logins_controller.rb
@@ -1,4 +1,6 @@
 class LoginsController < ApplicationController
+  skip_before_filter :authorize
+  
   def new
   end
   
