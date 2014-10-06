98aef49446a61937dc43086793cd4e2b47cef631
Tony Plaza <tplaza3@gmail.com>
2014-10-06 15:39:53 -0500

    update index

--- VisDir/app/controllers/listings_controller.rb
+++ VisDir/app/controllers/listings_controller.rb
@@ -2,6 +2,16 @@ class ListingsController < ApplicationController
   
   def index
     @listings = Listing.all
+    @users = User.all
+    
+    @acme = User.find(2)
+    @tiny = User.find(3)
+    @still = User.find(4)
+    @ansel = User.find(5)
+    @insta = User.find(6)
+  end
+  
+  def new
   end
   
 end
